fcd63973006ef6fac56fb7c086fc639e971365f0
Zak <zakplumridge@outlook.com>
2023-12-15 08:23:57 +0000

    Updated setup.sql
    Added input to create stored procedure in the setup.sql

--- database/setup.sql
+++ database/setup.sql
@@ -23,3 +23,19 @@ CREATE TABLE IF NOT EXISTS events (
     Description TEXT,
     FOREIGN KEY (OrganiserID) REFERENCES users(user_id)
 );
+
+-- Stored Procedure for User Registration
+
+CREATE PROCEDURE AddUser(
+    IN p_username VARCHAR(255),
+    IN p_email VARCHAR(255),
+    IN p_hashedPassword VARCHAR(255)
+)
+BEGIN
+    DECLARE hashedPassword VARCHAR(255);
+    
+    SET hashedPassword = p_hashedPassword;
+
+    INSERT INTO users (username, email, hashedPassword)
+    VALUES (p_username, p_email, hashedPassword);
+END //
